39728a4bcdb061d6df854908063ced2024e13c1e
shady-cj <petersp2000@gmail.com>
2024-01-21 11:47:40 +0100

    completed hero section

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,5 +1,8 @@
 import "./index.scss"
 import arrowLeft from "../../assets/arrow-left.svg"
+import bobPics from "../../assets/bob-pics.jpg";
+import heroImg from "../../assets/hero-img.png";
+import heroVid from "../../assets/hero-vid.mp4";
 const index = () => {
     return (
         <div className="hero-container">
@@ -18,8 +21,23 @@ const index = () => {
                 <p>Taking charge of both design and development, we craft websites <br /> that convert like spells without the hefty price tag.</p>
             </div>
             <div className="hero-contact">
-                <a className="start-a-project">Start a Project <img src={arrowLeft} alt="" /></a>
-                <a className="schedule-a-call">Schedule a Call</a>
+                <div>
+                    <a className="start-a-project">Start a Project <img src={arrowLeft} alt="" /></a>
+                    <a className="schedule-a-call">Schedule a Call</a>
+                </div>
+                <div className="hero-testimony-section">
+                    <img src={bobPics} alt="" />
+                    <div className="hero-testimony-info">
+                        <span>Bob Robson, Owner @Enver Flooring</span>
+                        <span>Their design magic transformed our website into a conversion machine! We saw a 20% increase in sales within 3 months.</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="hero-video">
+                <video muted loop autoPlay poster={heroImg}>
+                    <source src={heroVid} type="video/mp4" />
+                </video>
             </div>
 
         </div>
