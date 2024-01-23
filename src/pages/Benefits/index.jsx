508f10b9bca23581154cd2c866f1332f7970cffd
shady-cj <petersp2000@gmail.com>
2024-01-23 12:49:58 +0100

    completed benefits section and adjusted fonts of service section

--- src/pages/Benefits/index.jsx
+++ src/pages/Benefits/index.jsx
@@ -2,6 +2,11 @@ import "./index.scss";
 import magicPen from "../../assets/magicpen-yellow.svg";
 import stopwatch from "../../assets/stopwatch.svg";
 import nftImg from "../../assets/nft-img.jpg";
+import svgLines1 from "../../assets/svg-lines1.svg";
+import svgLines2 from "../../assets/svg-lines2.svg";
+import svg100 from "../../assets/svg-100.svg";
+import invoice from "../../assets/invoice.svg";
+import polygon from "../../assets/polygon.svg";
 const index = () => {
     return (
         <section className="benefit-container">
@@ -46,6 +51,74 @@ const index = () => {
                     </div>
                     <img src={nftImg} alt="" />
                     <div className="blur-circle-2">
+                    </div>
+                </article>
+                <article className="loading-time">
+                    <img src={svgLines1} alt="" />
+                    <img src={svgLines2} alt="" />
+                    <div className="lt-upper-rect">
+                        <span className="lt-green"></span>
+                        <span className="lt-blue"></span>
+                        <span className="lt-yellow"></span>
+                    </div>
+                    <div className="lt-metrics">
+                        <div>
+                            <img src={svg100} alt="" />
+                            <span>Performance</span>
+                        </div>
+                        <div>
+                            <img src={svg100} alt="" />
+                            <span>Accessibility</span>
+                        </div>
+                        <div>
+                            <img src={svg100} alt="" />
+                            <span>SEO</span>
+                        </div>
+                    </div>
+                    <div className="lt-content">
+                        <h2>Fast loading time</h2>
+                        <p>Patience is a virtue, but website loading shouldn&rsquo;t test it. We&rsquo;ll make your site load fast, keeping visitors happy.</p>
+                    </div>
+                </article>
+                <article className="revisions">
+                    <img src={svgLines1} alt="" />
+                    <img src={svgLines2} alt="" />
+                    <div className="blur-circle-3"></div>
+                    <h2>Unlimited Revisions</h2>
+                    <p>Tweak until your are satisfied! and of course with  no extra charge</p>
+                    <div className="rev-publish-card">
+                        <div>
+                            <span>Publish</span>
+                        </div>
+                        <span>You</span>
+                        <img src={polygon} alt="" />
+                    </div>
+                </article>
+                <article className="price-tags">
+                    <div className="blur-circle-4"></div>
+                    <h2>Quality Work Without the Hefty Price Tag</h2>
+
+                    <p>No need to break the bank for a beautiful website. </p>
+                    <div>
+                        <div className="pt-upper-rect">
+                            <span className="lt-green"></span>
+                            <span className="lt-blue"></span>
+                            <span className="lt-yellow"></span>
+                        </div>
+                        <div className="pt-price-content">
+                            <div className="pt-starting-from">
+                                <img src={invoice} alt="" />
+                                <span>starting from</span>
+                            </div>
+                            <div className="pt-price">
+                                <span>
+                                    $1828
+                                </span>
+                                <span>
+                                    $30,999
+                                </span>
+                            </div>
+                        </div>
                     </div>
                 </article>
             </div>
