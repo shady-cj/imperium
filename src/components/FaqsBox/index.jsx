7d4e3a3952fd5b39e5f32e0d7f767190f69e82a2
shady-cj <petersp2000@gmail.com>
2024-01-25 23:20:46 +0100

    working on faqs section

--- src/components/FaqsBox/index.jsx
+++ src/components/FaqsBox/index.jsx
@@ -1,18 +1,24 @@
 import "./index.scss";
+import { useState } from "react";
 import plus from "../../assets/plus.svg";
-
-const index = ({ question, answer }) => {
+import closeIcon from "../../assets/close-icon.svg";
+const Index = ({ question, answer }) => {
+    const [boxOpened, setBoxOpened] = useState(false);
     return (
 
-        <article className="question-box">
-            <p>{question}</p>
-            <img src={plus} alt="" />
-            <div className="answer-box">
+        <article className={`question-box ${boxOpened ? "opened" : "closed"}`}>
+            <div>
 
+                <p>{question}</p>
+                <img src={boxOpened ? closeIcon : plus} alt="" onClick={() => setBoxOpened(prev => !prev)} />
+            </div>
+
+            <div className={`answer-box`}>
+                {answer}
             </div>
         </article>
 
     )
 }
 
-export default index
+export default Index
