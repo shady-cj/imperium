6f97d2fb78c6fbbca9550df01bfa1e602a4e405d
shady-cj <petersp2000@gmail.com>
2024-01-28 13:53:06 +0100

    fixed faqs onopen animation

--- src/components/FaqsBox/index.jsx
+++ src/components/FaqsBox/index.jsx
@@ -2,20 +2,24 @@ import "./index.scss";
 import { useState } from "react";
 import plus from "../../assets/plus.svg";
 import closeIcon from "../../assets/close-icon.svg";
+import { motion } from "framer-motion"
 const Index = ({ question, answer }) => {
     const [boxOpened, setBoxOpened] = useState(false);
     return (
 
-        <article className={`question-box ${boxOpened ? "opened" : "closed"}`}>
+        <article className={`question-box`}>
             <div>
 
                 <p>{question}</p>
                 <img src={boxOpened ? closeIcon : plus} alt="" onClick={() => setBoxOpened(prev => !prev)} />
             </div>
+            <motion.div initial={{ height: "0rem" }} animate={boxOpened ? { height: "10rem" } : { height: "0rem" }} transition={{ type: "spring", ease: "ease-in-out" }}>
 
-            <div className={`answer-box`}>
-                {answer}
-            </div>
+                <div className={`answer-box`}>
+                    {answer}
+                </div>
+            </motion.div>
+
         </article>
 
     )
